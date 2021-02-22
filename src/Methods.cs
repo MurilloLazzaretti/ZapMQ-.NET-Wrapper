f917d8f748d9e2740e8c53849a771eebb48b3d10
MurilloLazzaretti <murillo@teclink.com.br>
2021-02-22 16:33:06 -0300

    General commit

--- src/Methods.cs
+++ src/Methods.cs
@@ -17,30 +17,86 @@ namespace ZapMQ
         public string GetMessage(string pQueueName)
         {
             HttpResponseMessage response = Connection.GetAsync("TZapMethods/GetMessage/" + pQueueName).Result;
-            string contentResponse = response.Content.ReadAsStringAsync().Result;
-            RootZapJSONMessage rootZapJSONResponse = JsonConvert.DeserializeObject<RootZapJSONMessage>(contentResponse);
-            return rootZapJSONResponse.result[0];
+            if (response.IsSuccessStatusCode)
+            {
+                string contentResponse = response.Content.ReadAsStringAsync().Result;
+                if (!string.IsNullOrEmpty(contentResponse)) 
+                {
+                    RootZapJSONMessage rootZapJSONResponse = JsonConvert.DeserializeObject<RootZapJSONMessage>(contentResponse);
+                    return rootZapJSONResponse.result[0];
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
         public string GetRPCMessage(string pQueueName, string pIdMessage)
         {
             HttpResponseMessage response = Connection.GetAsync("TZapMethods/GetRPCResponse/" + pQueueName + "/" + pIdMessage).Result;
-            string contentResponse = response.Content.ReadAsStringAsync().Result;
-            RootZapJSONMessage rootZapJSONResponse = JsonConvert.DeserializeObject<RootZapJSONMessage>(contentResponse);
-            return rootZapJSONResponse.result[0];
+            if (response.IsSuccessStatusCode)
+            {
+                string contentResponse = response.Content.ReadAsStringAsync().Result;
+                if (!string.IsNullOrEmpty(contentResponse))
+                {
+                    RootZapJSONMessage rootZapJSONResponse = JsonConvert.DeserializeObject<RootZapJSONMessage>(contentResponse);
+                    return rootZapJSONResponse.result[0];
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
         public string UpdateMessage(string pQueueName, string pMessage)
         {
             HttpResponseMessage response = Connection.GetAsync("TZapMethods/UpdateMessage/" + pQueueName + "/" + pMessage).Result;
-            string contentResponse = response.Content.ReadAsStringAsync().Result;
-            RootZapJSONMessage rootZapJSONResponse = JsonConvert.DeserializeObject<RootZapJSONMessage>(contentResponse);
-            return rootZapJSONResponse.result[0];
+            if (response.IsSuccessStatusCode)
+            {
+                string contentResponse = response.Content.ReadAsStringAsync().Result;
+                if (!string.IsNullOrEmpty(contentResponse))
+                {
+                    RootZapJSONMessage rootZapJSONResponse = JsonConvert.DeserializeObject<RootZapJSONMessage>(contentResponse);
+                    return rootZapJSONResponse.result[0];
+                }
+                else
+                {
+                    return string.Empty;
+                }    
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
         public string UpdateRPCResponse(string pQueueName, string pIdMessage, string pResponse)
         {
             HttpResponseMessage response = Connection.GetAsync("TZapMethods/UpdateRPCResponse/" + pQueueName + "/" + pIdMessage + "/" + pResponse).Result;
-            string contentResponse = response.Content.ReadAsStringAsync().Result;
-            RootZapJSONMessage rootZapJSONResponse = JsonConvert.DeserializeObject<RootZapJSONMessage>(contentResponse);
-            return rootZapJSONResponse.result[0];
+            if (response.IsSuccessStatusCode)
+            {
+                string contentResponse = response.Content.ReadAsStringAsync().Result;
+                if (!string.IsNullOrEmpty(contentResponse))
+                {
+                    RootZapJSONMessage rootZapJSONResponse = JsonConvert.DeserializeObject<RootZapJSONMessage>(contentResponse);
+                    return rootZapJSONResponse.result[0];
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
     }
 }
